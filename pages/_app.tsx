985adb1ba6ba0a9b5fc0f1789125e1673eb6ce97
griffin-a <griffinalmquist@gmail.com>
2022-06-16 23:10:10 +1200

    Basic portfolio skeleton

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// import "../styles/globals.css";
+import 'tailwindcss/tailwind.css'
+import type { AppProps } from "next/app";
+import Navigation from "../components/Navigation";
+import Footer from "../components/Footer";
+import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <ThemeProvider attribute="class" enableSystem={false}>
+        <Navigation />
+        <Component {...pageProps} />
+        <Footer />
+      </ThemeProvider>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
