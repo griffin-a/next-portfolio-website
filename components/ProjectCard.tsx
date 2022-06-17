f1c6735fe02a6301412dec51e9c3f131886a1e08
griffin-a <griffinalmquist@gmail.com>
2022-06-17 21:18:28 +1200

    better color contrast for form submit button

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -33,7 +33,7 @@ const ProjectCard = ({
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             {title}
           </h5>
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-200">
             {description}
           </p>
         </div>
--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -68,7 +68,7 @@ const Contact = () => {
         <div className="md:flex md:items-center">
           <div className="md:w-1/3">
             <button
-              className="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+              className="shadow bg-blue-600 hover:bg-blue-700 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
               type="button"
               onClick={() => console.log("Submitted")}
             >
