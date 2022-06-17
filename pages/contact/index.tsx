0f643a30b220f2991b6f87995244888a88702f46
griffin-a <griffinalmquist@gmail.com>
2022-06-17 20:42:06 +1200

    basic project skeleton card

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -7,7 +7,7 @@ const Contact = () => {
       <form className="mx-auto text-left w-full max-w-lg">
         <div className="flex flex-wrap -mx-3 mb-6">
           <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 dark:text-white">
               First Name
             </label>
             <input
@@ -22,7 +22,7 @@ const Contact = () => {
             </p> */}
           </div>
           <div className="w-full md:w-1/2 px-3">
-            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 dark:text-white">
               Last Name
             </label>
             <input
@@ -36,7 +36,7 @@ const Contact = () => {
         </div>
         <div className="flex flex-wrap -mx-3 mb-6">
           <div className="w-full px-3">
-            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 dark:text-white">
               E-mail
             </label>
             <input
@@ -45,21 +45,21 @@ const Contact = () => {
               type="email"
               required={true}
             />
-            <p className="text-gray-600 text-xs italic">
+            <p className="text-gray-600 text-xs italic dark:text-white">
               Some tips - as long as needed
             </p>
           </div>
         </div>
         <div className="flex flex-wrap -mx-3 mb-6">
           <div className="w-full px-3">
-            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 dark:text-white">
               Message
             </label>
             <textarea
               className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
               id="message"
             ></textarea>
-            <p className="text-gray-600 text-xs italic">
+            <p className="text-gray-600 text-xs italic dark:text-white">
               Re-size can be disabled by set by resize-none / resize-y /
               resize-x / resize
             </p>
