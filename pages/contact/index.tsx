1b6d38266b5ff1d5389d7060356c0681cf7a27e8
griffin-a <griffinalmquist@gmail.com>
2022-06-17 20:03:51 +1200

    basic contact form

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,5 +1,85 @@
 const Contact = () => {
-
-}
+  return (
+    <div>
+      <h3 className="text-2xl font-bold text-gray-900 lg:text-5xl dark:text-white text-center py-10">
+        Contact   
+      </h3>
+      <form className="mx-auto text-left w-full max-w-lg">
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+              First Name
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              id="grid-first-name"
+              type="text"
+              placeholder="Jane"
+              required={true}
+            />
+            {/* <p className="text-red-500 text-xs italic">
+              Please fill out this field.
+            </p> */}
+          </div>
+          <div className="w-full md:w-1/2 px-3">
+            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+              Last Name
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-last-name"
+              type="text"
+              placeholder="Doe"
+              required={true}
+            />
+          </div>
+        </div>
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
+            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+              E-mail
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="email"
+              type="email"
+              required={true}
+            />
+            <p className="text-gray-600 text-xs italic">
+              Some tips - as long as needed
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
+            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+              Message
+            </label>
+            <textarea
+              className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
+              id="message"
+            ></textarea>
+            <p className="text-gray-600 text-xs italic">
+              Re-size can be disabled by set by resize-none / resize-y /
+              resize-x / resize
+            </p>
+          </div>
+        </div>
+        <div className="md:flex md:items-center">
+          <div className="md:w-1/3">
+            <button
+              className="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+              type="button"
+              onClick={() => console.log("Submitted")}
+            >
+              Send
+            </button>
+          </div>
+          <div className="md:w-2/3"></div>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default Contact;
