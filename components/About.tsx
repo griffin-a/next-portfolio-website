17426b9088051f9c6b7c8e0522de0622e96231e7
griffin-a <griffinalmquist@gmail.com>
2022-06-17 22:05:02 +1200

    fa icons; not formatted yet though

--- components/About.tsx
+++ components/About.tsx
@@ -1,38 +1,67 @@
-import React from "react"
-import Image from "next/image"
-import profile from "../public/profile.jpg"
+import React from "react";
+import Image from "next/image";
+import profile from "../public/profile.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faLinkedin,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
 
 const About = () => {
-    return (
-        <div className="container px-4 mx-auto">
-            <div className="lg:space-x-5 lg:flex lg:flex-row item-center lg:-mx-4 flex flex-col-reverse text-center lg:text-left">
-                <div className="lg:px-4 lg:mt-12 ">
-                    <h1 className="text-2xl font-bold text-gray-900 lg:text-5xl dark:text-white">
-                        Hey there, I'm Griffin
-                    </h1>
-                    <div className="mt-6 text-gray-800 dark:text-white">
-                        <p className="mb-4">
-                            My name is Griffin Almquist.
-                            I am a final-year Computer Science student at the University of Auckland.
-                            Welcome to my portfolio website; here you can find out what I have been working on recently
-                            and how you can contact me!
-                        </p>
-                    </div>
-                </div>
-                <div className="flex-shrink-0 lg:mt-12 lg:px-4 mb-10">
-                    <Image
-                        src={profile}
-                        alt="Profile"
-                        priority={true}
-                        className="rounded-full"
-                        width={250}
-                        height={250}
-                        placeholder="blur"
-                    />
-                </div>
-            </div>
+  return (
+    <div className="container px-4 mx-auto">
+      <div className="lg:space-x-5 lg:flex lg:flex-row item-center lg:-mx-4 flex flex-col-reverse text-center lg:text-left">
+        <div className="lg:px-4 lg:mt-12 ">
+          <h1 className="text-2xl font-bold text-gray-900 lg:text-5xl dark:text-white">
+            Hey there, I'm Griffin
+          </h1>
+          <div className="mt-6 text-gray-800 dark:text-white">
+            <p className="mb-4">
+              My name is Griffin Almquist. I am a final-year Computer Science
+              student at the University of Auckland. Welcome to my portfolio
+              website; here you can find out what I have been working on
+              recently and how you can contact me!
+            </p>
+          </div>
         </div>
-    )
-}
+        <div className="flex-shrink-0 lg:mt-12 lg:px-4 mb-10">
+          <Image
+            src={profile}
+            alt="Profile"
+            priority={true}
+            className="rounded-full"
+            width={250}
+            height={250}
+            placeholder="blur"
+          />
+
+          <div className="flex mx-auto text-center">
+            <a href="#facebook">
+              <FontAwesomeIcon
+                icon={faFacebook}
+                style={{ fontSize: 40, paddingRight: 20 }}
+              />
+            </a>
+
+            <a href="#linkedin">
+              <FontAwesomeIcon
+                icon={faLinkedin}
+                style={{ fontSize: 40, paddingRight: 20 }}
+              />
+            </a>
+
+            <a href="#github">
+              <FontAwesomeIcon
+                icon={faGithub}
+                style={{ fontSize: 40, paddingRight: 20 }}
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default About;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
-// import "../styles/globals.css";
 import 'tailwindcss/tailwind.css'
 import type { AppProps } from "next/app";
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
 import { ThemeProvider } from "next-themes";
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
