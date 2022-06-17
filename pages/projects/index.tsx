a71740d57750d6c7472ae79cb6aa49e2fde9e541
griffin-a <griffinalmquist@gmail.com>
2022-06-17 22:56:15 +1200

    Added 331 project to projects

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -20,6 +20,28 @@ const Projects = () => {
         WDCC club at the University of Auckland. My role is a front-end developer. Is yet to be completed."
         projectUrl="#"
       />
+
+      <ProjectCard
+        imageName="todo.jpeg"
+        title="Todo List Web-App"
+        description="A full-stack todo list web-app that provides full CRUD functionality and connection to a database.
+        Uses authentication and authorization to create and use accounts."
+        projectUrl="#"
+      />
+
+      <ProjectCard
+        imageName="co2.jpeg"
+        title="Carbon Emissions Calculator"
+        description="A proof of concept design for a carbon emissions calculator. A high-fidelity prototype."
+        projectUrl="#"
+      />
+
+      <ProjectCard
+        imageName="concert.jpeg"
+        title="Concert Booking Website"
+        description="A full-stack concert booking service that supports registering and logging in. Group project"
+        projectUrl="#"
+      />
     </div>
   );
 };
