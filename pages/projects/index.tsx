fbfea91f5cb60f5c1ff9db2bab93218da321df95
griffin-a <griffinalmquist@gmail.com>
2022-08-14 22:50:59 +1200

    updates

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -10,7 +10,7 @@ const Projects = () => {
              the program finds the shortest path. Provides the ability to use
              different pathfinding algorithms as well as different maze
              generation algorithms."
-        projectUrl="#"
+        projectUrl="https://graph-algo-visualizer-griffin-a.netlify.app/"
       />
 
       <ProjectCard
@@ -18,7 +18,7 @@ const Projects = () => {
         title="Investment Island"
         description="An ongoing group project that I am working on at the moment with the 
         WDCC club at the University of Auckland. My role is a front-end developer. Is yet to be completed."
-        projectUrl="#"
+        projectUrl="https://github.com/UoaWDCC/Investment-island"
       />
 
       <ProjectCard
@@ -33,15 +33,23 @@ const Projects = () => {
         imageName="co2.jpeg"
         title="Carbon Emissions Calculator"
         description="A proof of concept design for a carbon emissions calculator. A high-fidelity prototype."
-        projectUrl="#"
+        projectUrl="https://griffin-a.github.io/345-a2/"
       />
 
       <ProjectCard
+        imageName="library.jpeg"
+        title="Book Library Browser"
+        description="A full-stack application that provides the ability to browse and search for books in a
+        database and create/sign in to accounts."
+        projectUrl="https://book-web-app-235.herokuapp.com/"
+      />
+
+      {/* <ProjectCard
         imageName="concert.jpeg"
         title="Concert Booking Website"
         description="A full-stack concert booking service that supports registering and logging in. Group project"
         projectUrl="#"
-      />
+      /> */}
     </div>
   );
 };
